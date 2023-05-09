751678e41b41f982dc28cffa1b99cdf208bd1ada
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-05-08 23:09:38 -0500

    printing midi inputs

--- synth.py
+++ synth.py
@@ -1,3 +1,11 @@
 import mido
 
-print(mido.get_input_names())
+ports = mido.get_input_names()
+print(ports)
+
+port_name = ports[1]
+port = mido.open_input(port_name)
+
+while True:
+    for message in port.iter_pending():
+        print(message)
