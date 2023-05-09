2f2dc517ffb2cca8d8e96500734387b73c6a73f2
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-05-08 23:38:23 -0500

    sin wave playback

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 pip install mido==1.2.9
 pip install python-rtmidi
+pip install numpy
+pip install --upgrade sounddevice
--- synth.py
+++ synth.py
@@ -1,4 +1,23 @@
 import mido
+import numpy as np
+import sounddevice as sd
+
+# Constants for audio generation
+sample_rate = 44100
+duration = 0.2
+
+# MIDI callback function
+def midi_callback(message):
+    if message.type == 'note_on':
+        frequency = 440 * (2 ** ((message.note - 69) / 12))
+        t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+        audio = np.sin(2 * np.pi * frequency * t)  # Sawtooth wave generation
+
+        # Play the audio in real-time
+        sd.play(audio, sample_rate)
+    elif message.type == 'note_off':
+        # Stop playing the audio
+        sd.stop()
 
 ports = mido.get_input_names()
 print(ports)
@@ -6,6 +25,15 @@ print(ports)
 port_name = ports[1]
 port = mido.open_input(port_name)
 
-while True:
-    for message in port.iter_pending():
-        print(message)
+# Set the MIDI callback function
+port.callback = midi_callback
+
+# Keep the program running
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pass
+
+# Close the MIDI port
+port.close()
