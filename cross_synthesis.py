cafa060034ff4e74af43afbd117c1cec2bb305c6
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-05-11 10:03:00 -0500

    cross synthesis module

---
+++ cross_synthesis.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import librosa
+
+def stft( input_sound, dft_size, hop_size, zero_pad, window):
+    f = []
+    for i in range(0, len(input_sound), hop_size):
+        if i+dft_size > len(input_sound):
+            vec = input_sound[i:]
+        else:
+            vec = input_sound[i:i+dft_size]
+        vec = np.pad(vec, (0, dft_size-len(vec)), 'constant') * np.array(window)
+        f.append(np.fft.rfft(vec, n=dft_size+zero_pad))
+        
+    # Return a complex-valued spectrogram (frequencies x time)
+    return np.array(f, dtype=complex)
+
+def istft( stft_output, dft_size, hop_size, zero_pad, window):
+    isft = np.fft.irfft(stft_output, n=dft_size+zero_pad) * np.array(window)
+    n_frames = len(stft_output)
+    x = np.zeros(n_frames*hop_size+dft_size+zero_pad)
+    for i in range(n_frames):
+        x[i*hop_size:i*hop_size+dft_size+zero_pad] += isft[i]
+    # Return reconstructed waveform
+    return x
+
+def compute_spectral_envelope(stft_matrix, smoothing_window_size, downsampling_factor):
+    # Step 1: Compute the magnitude spectrum
+    magnitude_spectrum = np.abs(stft_matrix)
+
+    # Step 2: Smooth the magnitude spectrum
+    window = np.ones(smoothing_window_size) / smoothing_window_size
+    smoothed_spectrum = np.apply_along_axis(lambda m: np.convolve(m, window, mode='same'), axis=1, arr=magnitude_spectrum)
+
+    # Step 3: Downsample the smoothed spectrum
+    downsampled_spectrum = smoothed_spectrum[:, ::downsampling_factor]
+
+    # Step 4: Interpolate the downsampled spectrum
+    interpolated_spectrum = np.repeat(downsampled_spectrum, downsampling_factor, axis=1)
+
+    # Step 5: Normalize the spectral envelope
+    spectral_envelope = interpolated_spectrum / np.max(interpolated_spectrum)
+
+    return spectral_envelope
